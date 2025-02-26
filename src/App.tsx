7a21d6cd8833a5db137ce7ca9552cc7dc1c44c48
Julia T <juliaturkali@gmail.com>
2025-02-26 10:35:54 +0100

    Create + Design Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Header from "./components/Header";
+
 function App() {
-  return <></>;
+  return (
+    <section className="flex h-screen flex-col items-center bg-slate-800 text-slate-100">
+      <Header />
+    </section>
+  );
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header>
+      <h1 className="my-10 flex gap-10 text-center text-5xl font-bold tracking-widest uppercase">
+        <span>Rock</span> <span>|</span>
+        <span>Paper</span>
+        <span>|</span> <span>Scissor</span>
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
