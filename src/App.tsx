74ed5ce30370b81783f732f90905c54f9c87a38c
Julia T <juliaturkali@gmail.com>
2025-02-26 11:38:25 +0100

    Design GamePlay

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import ScoreBoard from "./components/ScoreBoard";
+import GamePlay from "./components/GamePlay";
 
 function App() {
   const [playerScore, setPlayerScore] = useState(0);
   const [computerScore, setComputerScore] = useState(0);
+  const [playerChoice, setPlayerChoice] = useState("");
+  const [computerChoice, setComputerChoice] = useState("");
   return (
     <section className="flex h-screen flex-col items-center bg-slate-800 text-slate-100">
       <Header />
       <ScoreBoard playerScore={playerScore} computerScore={computerScore} />
+      <GamePlay />
     </section>
   );
 }
---
+++ src/components/GamePlay.tsx
@@ -0,0 +1,34 @@
+const GamePlay = () => {
+  const buttonDesign =
+    "size-50 rounded-full border-8 border-amber-400 bg-slate-700 p-8 cursor-pointer transition-all duration-300 ease-in-out hover:bg-slate-300 hover:shadow-lg flex items-center justify-center group";
+  return (
+    <section className="flex flex-col items-center gap-10">
+      <h2 className="text-4xl font-bold">Make your Choice</h2>
+      <div className="choices mt-8 flex justify-around gap-20">
+        <button className={`${buttonDesign} `}>
+          <img
+            src="src/assets/img/rock.svg"
+            alt="Rock"
+            className="size-30 rotate-270 transition-transform duration-300 group-hover:scale-130"
+          />
+        </button>
+        <button className={buttonDesign}>
+          <img
+            src="src/assets/img/paper.svg"
+            alt="Paper"
+            className="size-30 transition-transform duration-300 group-hover:scale-130"
+          />
+        </button>
+        <button className={buttonDesign}>
+          <img
+            src="src/assets/img/scissors.svg"
+            alt="Scissors"
+            className="size-30 transition-transform duration-300 group-hover:scale-130"
+          />
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default GamePlay;
